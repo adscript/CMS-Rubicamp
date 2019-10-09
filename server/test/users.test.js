8c5bf65669f256e14b83d4dbb22472f91392e854
adscript <adnanocode@gmail.com>
2019-10-10 01:44:28 +0700

    add user list unit test

---
+++ server/test/users.test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+
+const server = require('../app');
+const User = require("../models/user");
+
+const should = chai.should();
+chai.use(chaiHTTP);
+
+describe('users', function () {
+
+    // setiap sebelum melakukan test, table ditambahkan satu data "belajar TDD"
+    beforeEach(function (done) {
+        let user = new User({
+            email: "adnanradjam@gmail.com",
+            password: "AdnanGanteng12#",
+            token: ""
+        });
+
+        user.save(function (err) {
+            if(err) console.log(err)
+            else
+               done();
+        })
+    })
+
+    // setiap setelah melakukan test, table di drop
+    afterEach(function (done) {
+        User.collection.drop();
+        done();
+    });
+
+
+    it('seharusnya mendapatkan semua daftar User yang ada di table users dengan metode GET', function(done){
+        chai.request(server)
+        .get('/api/users/list')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body[0].should.have.property('_id');
+          res.body[0].should.have.property('email');
+          res.body[0].should.have.property('password');
+          res.body[0].should.have.property('token');
+          res.body[0].email.should.equal('adnanradjam@gmail.com');
+          done();
+        })
+      })
+
+})

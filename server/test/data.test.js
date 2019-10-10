b91b5d407e093b167bd06c32653dd77030436978
adscript <adnanocode@gmail.com>
2019-10-10 22:04:13 +0700

    complete data.test

--- server/test/data.test.js
+++ server/test/data.test.js
@@ -136,4 +136,85 @@ describe('data', function () {
     });
     // ============================================ END EDIT DATA TEST =====================================================
 
+    // ============================================ ADD DATA TEST ==========================================================
+    it('seharusnya berhasil tambah data  di table data dengan metode POST', function (done) {
+        chai.request(server)
+            .post(`/api/data`)
+            .send({
+                letter: "B",
+                frequency: 1.2
+            })
+            .end(function (err, response) {
+                response.should.have.status(201);
+                response.body.should.be.a('object');
+                response.body.should.have.property('success');
+                response.body.should.have.property('message');
+                response.body.should.have.property('data');
+                response.body.data.should.have.property('_id');
+                response.body.data.should.have.property('letter');
+                response.body.data.should.have.property('frequency');
+                response.body.data.frequency.should.equal(1.2);
+                response.body.data.letter.should.equal('B');
+                done();
+            })
+    });
+    // ============================================ END ADD DATA TEST ==========================================================
+
+    // ============================================ DELETE DATA TEST ==========================================================
+    it('Seharusnya berhasil hapus data di table data dengan metode GET', function (done) {
+        chai.request(server)
+            .post(`/api/data/search`)
+            .send({
+                letter: 'A'
+            })
+            .end(function (err, data) {
+                chai.request(server)
+                    .delete(`/api/data/${data.body[0]._id}`)
+                    .end(function (err, response) {
+                        response.should.have.status(201);
+                        response.body.should.be.a('object');
+                        response.body.should.have.property('success');
+                        response.body.should.have.property('message');
+                        response.body.should.have.property('data');
+                        response.body.data.should.have.property('_id');
+                        response.body.data.should.have.property('letter');
+                        response.body.data.should.have.property('frequency');
+                        response.body.success.should.equal(true);
+                        response.body.message.should.equal("data have been deleted");
+                        response.body.data.frequency.should.equal(1.1);
+                        response.body.data.letter.should.equal('A');
+                        done();
+                    })
+            })
+    })
+    // ============================================ END DELETE DATA TEST ==========================================================
+
+    // ============================================ FIND DATA TEST ==========================================================
+    it('Seharusnya berhasil hapus data di table data dengan metode GET', function (done) {
+        chai.request(server)
+            .post(`/api/data/search`)
+            .send({
+                letter: 'A'
+            })
+            .end(function (err, data) {
+                chai.request(server)
+                    .get(`/api/data/${data.body[0]._id}`)
+                    .end(function (error, response) {
+                        response.should.have.status(200);
+                        response.body.should.be.a('object');
+                        response.body.should.have.property('success');
+                        response.body.should.have.property('message');
+                        response.body.should.have.property('data');
+                        response.body.data.should.have.property('_id');
+                        response.body.data.should.have.property('letter');
+                        response.body.data.should.have.property('frequency');
+                        response.body.success.should.equal(true);
+                        response.body.message.should.equal("data found");
+                        response.body.data.frequency.should.equal(1.1);
+                        response.body.data.letter.should.equal('A');
+                        done();
+                    })
+            })
+    })
+    // ============================================ END FIND DATA TEST ==========================================================
 })

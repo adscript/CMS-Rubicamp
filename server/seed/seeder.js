03eb83bd9369eee36e83b4f8a8f2b3e1f6dfcb64
adscript <adnanocode@gmail.com>
2019-10-16 22:29:54 +0700

    add seed, to import data.json to mongoose

---
+++ server/seed/seeder.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cmsdb', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const dataDate = require('../models/datadate');
+
+
+const fs = require('fs');
+
+fs.readFile("./seed/data.json","utf8", (err, data) => {
+    if(err) 
+        throw err;
+    let list = JSON.parse(data);
+    dataDate.insertMany(list, () => console.log("Import Completed"));
+});
